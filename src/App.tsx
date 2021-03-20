b757df4b35b2dc2b4d5639809f6efa01867ee776
João Victor Ipirajá de Alencar <joaovictorpiraja@gmail.com>
2021-03-20 16:31:35 -0300

    Add style

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import styled from 'styled-components'
+import { GlobalStyle } from "./styles/global";
 
-const Title = styled.h1`
-  color: #8257e6;
-  font-size: 64px;
-`
+
 
 export function App() {
   return (
     <div className="App">
-      <Title>Olá,mundo!</Title>
+      <h1>Olá,mundo!</h1>
+      <GlobalStyle />
     </div>
   );
 }
